c2dbb5abeb0c5a004490a7ff88ee892532e86ba4
Newman-Nheeon <newmanenyoojo@gmail.com>
2022-11-08 15:59:54 +0100

    User Authentication on Register

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
+const joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
 
 const userSchema = new mongoose.Schema({
     firstName: {type:String, required:true},
@@ -12,3 +14,16 @@ userSchema.methods.generateAuthToken = function () {
     const token = jwt.sign({_id:this._id}, process.env.JWTPRIVATEKEY, {expiresIn:"7d"})
     return token
 };
+
+const User = mongoose.model('user', userSchema);
+const validate = (data) => {
+    const schema = joi.object({
+        firstName: joi.string().required().label("First Name"),
+        lastName: joi.string().required().label("Last Name"),
+        email: joi.string().required().label("email"),
+        password: passwordComplexity.required().label("Password"),
+    });
+    return schema.validate(data)
+};
+
+module.exports = {User, validate}
